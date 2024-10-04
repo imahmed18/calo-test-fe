b661339ec56b14bd151d5e14de8bd0596c58f29d
ahmed <imahmed18@gmail.com>
2024-10-04 18:17:39 +0500

    added home link to navbar logo

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,11 @@
 import { Navbar } from 'flowbite-react';
+import { Link } from 'react-router-dom';
 
 export function NavbarComponent() {
   return (
     <header>
       <Navbar fluid className="bg-gray-500">
-        <Navbar.Brand href="https://flowbite-react.com">
+        <Navbar.Brand as={Link} to="/">
           <img src="/logo192.png" className="mr-3 h-6 sm:h-9" alt="Flowbite React Logo" />
           <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
             Calo Test Project
